a9ac314f2e44605c6847f8f3f50d0283c0eb7975
Amit Patel <amit@innovations20.com>
2022-02-19 12:37:07 +0530

    show admin file in post

--- post/admin.py
+++ post/admin.py
@@ -3,34 +3,34 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-#
-# class PostUploadAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'user', 'message', 'is_view', 'is_like', 'is_share', 'id', 'create_at')
-#     list_filter = ('create_at',)
-#
-#
-# admin.site.register(PostUpload, PostUploadAdmin)
-#
-#
-# class PostLikeAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'post_id', 'id', 'create_at')
-#     list_filter = ('create_at',)
-#
-#
-# admin.site.register(PostLike, PostLikeAdmin)
-#
-#
-# class PostViewAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'post_id', 'id', 'create_at')
-#     list_filter = ('create_at',)
-#
-#
-# admin.site.register(PostView, PostViewAdmin)
-#
-#
-# class PostShareAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'post_id', 'id', 'create_at')
-#     list_filter = ('create_at',)
-#
-#
-# admin.site.register(PostShare, PostShareAdmin)
+
+class PostUploadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'message', 'is_view', 'is_like', 'is_share', 'id', 'create_at')
+    list_filter = ('create_at',)
+
+
+admin.site.register(PostUpload, PostUploadAdmin)
+
+
+class PostLikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post_id', 'id', 'create_at')
+    list_filter = ('create_at',)
+
+
+admin.site.register(PostLike, PostLikeAdmin)
+
+
+class PostViewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post_id', 'id', 'create_at')
+    list_filter = ('create_at',)
+
+
+admin.site.register(PostView, PostViewAdmin)
+
+
+class PostShareAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post_id', 'id', 'create_at')
+    list_filter = ('create_at',)
+
+
+admin.site.register(PostShare, PostShareAdmin)
