b69aec7fc361e4c3e379110bd8bba8569071cbf5
dev1 <dev1@gmail.com>
2023-02-21 16:48:52 +0530

    add match profile

--- matchprofile/views.py
+++ matchprofile/views.py
@@ -98,11 +98,11 @@ class UserFilterApiView(ListAPIView):
                     val = User.objects.filter(Q(is_complete_profile=True) & Q(birth_date__gte=max_date,
                                        birth_date__lte=min_date)).filter(gender__in =eval(self.request.GET['interest_in']) ,location__dwithin=(user_location, distance_to_decimal_degrees(D(km=related_distance)))).annotate(distance = Distance("location", user_location)).order_by("create_at").exclude(id=user_info[0].id)
                 else :
-                    val = User.objects.filter(Q(is_complete_profile=True) & Q(birth_date__gte=max_date,
+                    val = User.objects.filter(Q(passion__in=user_info[0].passion.all()) & Q(interest_in=user_info[0].gender) & Q(is_complete_profile=True) & Q(birth_date__gte=max_date,
                                        birth_date__lte=min_date)).filter(location__dwithin=(user_location, distance_to_decimal_degrees(D(km=related_distance)))).annotate(distance = Distance("location", user_location)).order_by("create_at").exclude(id=user_info[0].id)
             
             else :
-                val = User.objects.filter(Q(is_complete_profile=True) & Q(birth_date__gte=max_date,
+                val = User.objects.filter(Q(passion__in=user_info[0].passion.all()) & Q(interest_in=user_info[0].gender) & Q(is_complete_profile=True) & Q(birth_date__gte=max_date,
                                        birth_date__lte=min_date)).filter(location__dwithin=(user_location, distance_to_decimal_degrees(D(km=related_distance)))).annotate(distance = Distance("location", user_location)).order_by("create_at").exclude(id=user_info[0].id)
               
             return val
@@ -111,7 +111,7 @@ class UserFilterApiView(ListAPIView):
             user_info[0].birth_date.year - 10))
             & Q(birth_date__year__lte=(
                 user_info[0].birth_date.year + 10)) &
-            Q(gender__in=user_info[0].interest_in) |
+            # Q(gender__in=user_info[0].interest_in) |
             Q(interest_in__in=user_info[0].gender) &
             Q(city=user_info[0].city) |
             Q(location=user_info[0].location) |
